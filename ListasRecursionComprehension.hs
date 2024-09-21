767452facbbae4ee35ba08de816665ec77c1de85
Sotelord1113 <hector.sotelo@ciencias.unam.mx>
2024-09-20 22:28:47 -0700

    Add files via upload

--- ListasRecursionComprehension.hs
+++ ListasRecursionComprehension.hs
@@ -7,24 +7,24 @@ sumaLista [] = 0
 sumaLista (x:xs) = x + sumaLista(xs)
 
 agregaElemento :: [a] -> a -> Bool -> [a]
-agregaElemento (x:xs) a agregar = if agregar==True 
-                                then a : (x:xs)
-                                else (x:xs)++[a]
-agregaElemento [] a agregar = if agregar==True 
-                                then a : []
-                                else []++[a]
+agregaElemento [] elem _ = [elem]
+agregaElemento (x:xs) elem bool = if bool
+                                  then elem : (x:xs)
+                                  else (x:xs) ++ [elem]
 
 maximoLista :: (Num a, Ord a) => [a] -> a
 maximoLista [x] = x
-maximoLista (x:xs) = max x (maximoLista xs)
+maximoLista (x:xs) = if x > maximoLista xs
+                        then x
+                        else maximoLista xs
 
 indice :: [a] -> Int -> a
 indice [] _ = error "Lista vacia"
-indice (x:xs) y = if y<0 || y>longitud (x:xs)  
+indice (x:xs) elem = if elem < 0 || elem >= longitud (x:xs)  
                     then error "Índice fuera de rango"
-                    else if y == 0
+                    else if elem == 0
                             then x
-                            else indice xs (y-1)
+                            else indice xs (elem-1)
 
 divisores :: Int -> [Int]
 divisores n = [x | x <- [1..n], n `mod` x == 0]
@@ -35,6 +35,4 @@ conjunto (x:xs) = x : conjunto [y|y <- xs, y /= x]
 
 numerosPares :: [Int] -> [Int]
 numerosPares [] = []
-numerosPares (x:xs) = if x `mod` 2 ==0
-                    then x: numerosPares xs
-                    else numerosPares xs
+numerosPares (x:xs) = [x | x <- (x:xs), x `mod` 2 == 0]
